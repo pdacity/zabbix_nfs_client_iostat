cc18d4e7e320bb4f615fb900814296ef82a9fd64
Dmitry Malinin <pdacity@ya.ru>
2020-06-13 11:36:43 +0300

    Update nfsio_perf.sh

--- nfsio_perf.sh
+++ nfsio_perf.sh
@@ -44,67 +44,67 @@ CMD=/usr/sbin/nfsiostat
 
 case ${NFS_IO} in
     op_s)
-        result=`$CMD $NFS_MOUNT 1 3| grep -A 1 'rpc bklog' |tail -1| awk -F' ' '{print $1}'`
+        result=`$CMD 1 3 $NFS_MOUNT| grep -A 1 'rpc bklog' |tail -1| awk -F' ' '{print $1}'`
         echo ${result}
         ;;
     bklog)
-        result=`$CMD $NFS_MOUNT 1 3| grep -A 1 'rpc bklog' |tail -1| awk -F' ' '{print $2}'`
+        result=`$CMD 1 3 $NFS_MOUNT| grep -A 1 'rpc bklog' |tail -1| awk -F' ' '{print $2}'`
         echo ${result}
         ;;
     read_ops_s)
-        result=`$CMD $NFS_MOUNT 1 3| grep -A 1 '^read:' |tail -1| awk -F' ' '{print $1}'`
+        result=`$CMD 1 3 $NFS_MOUNT| grep -A 1 '^read:' |tail -1| awk -F' ' '{print $1}'`
         echo ${result}
         ;;
     read_kB_s)
-        result=`$CMD $NFS_MOUNT 1 3| grep -A 1 '^read:' |tail -1| awk -F' ' '{print $2}'`
+        result=`$CMD 1 3 $NFS_MOUNT| grep -A 1 '^read:' |tail -1| awk -F' ' '{print $2}'`
         echo ${result}
         ;;
     read_kB_op)
-        result=`$CMD $NFS_MOUNT 1 3| grep -A 1 '^read:' |tail -1| awk -F' ' '{print $3}'`
+        result=`$CMD 1 3 $NFS_MOUNT| grep -A 1 '^read:' |tail -1| awk -F' ' '{print $3}'`
         echo ${result}
         ;;
     read_retry)
-        result=`$CMD $NFS_MOUNT 1 3| grep -A 1 '^read:' |tail -1| awk -F' ' '{print $4}'`
+        result=`$CMD 1 3 $NFS_MOUNT| grep -A 1 '^read:' |tail -1| awk -F' ' '{print $4}'`
         echo ${result}
         ;;
     read_retry_perc)
-        result=`$CMD $NFS_MOUNT 1 3| grep -A 1 '^read:' |tail -1| awk -F' ' '{print $5}' | grep -o '[^(].*[^%)]'`
+        result=`$CMD 1 3 $NFS_MOUNT| grep -A 1 '^read:' |tail -1| awk -F' ' '{print $5}' | grep -o '[^(].*[^%)]'`
         echo ${result}
         ;;
     read_rtt)
-        result=`$CMD $NFS_MOUNT 1 3| grep -A 1 '^read:' |tail -1| awk -F' ' '{print $6}'`
+        result=`$CMD 1 3 $NFS_MOUNT| grep -A 1 '^read:' |tail -1| awk -F' ' '{print $6}'`
         echo ${result}
         ;;
     read_exe)
-        result=`$CMD $NFS_MOUNT 1 3| grep -A 1 '^read:' |tail -1| awk -F' ' '{print $7}'`
+        result=`$CMD 1 3 $NFS_MOUNT| grep -A 1 '^read:' |tail -1| awk -F' ' '{print $7}'`
         echo ${result}
         ;;
     write_ops_s)
-        result=`$CMD $NFS_MOUNT 1 3| grep -A 1 '^write:' |tail -1| awk -F' ' '{print $1}'`
+        result=`$CMD 1 3 $NFS_MOUNT| grep -A 1 '^write:' |tail -1| awk -F' ' '{print $1}'`
         echo ${result}
         ;;
     write_kB_s)
-        result=`$CMD $NFS_MOUNT 1 3| grep -A 1 '^write:' |tail -1| awk -F' ' '{print $2}'`
+        result=`$CMD 1 3 $NFS_MOUNT| grep -A 1 '^write:' |tail -1| awk -F' ' '{print $2}'`
         echo ${result}
         ;;
     write_kB_op)
-        result=`$CMD $NFS_MOUNT 1 3| grep -A 1 '^write:' |tail -1| awk -F' ' '{print $3}'`
+        result=`$CMD 1 3 $NFS_MOUNT| grep -A 1 '^write:' |tail -1| awk -F' ' '{print $3}'`
         echo ${result}
         ;;
     write_retry)
-        result=`$CMD $NFS_MOUNT 1 3| grep -A 1 '^write:' |tail -1| awk -F' ' '{print $4}'`
+        result=`$CMD 1 3 $NFS_MOUNT| grep -A 1 '^write:' |tail -1| awk -F' ' '{print $4}'`
         echo ${result}
         ;;
     write_retry_perc)
-        result=`$CMD $NFS_MOUNT 1 3| grep -A 1 '^write:' |tail -1| awk -F' ' '{print $5}' | grep -o '[^(].*[^%)]'`
+        result=`$CMD 1 3 $NFS_MOUNT| grep -A 1 '^write:' |tail -1| awk -F' ' '{print $5}' | grep -o '[^(].*[^%)]'`
         echo ${result}
         ;;
     write_rtt)
-        result=`$CMD $NFS_MOUNT 1 3| grep -A 1 '^write:' |tail -1| awk -F' ' '{print $6}'`
+        result=`$CMD 1 3 $NFS_MOUNT| grep -A 1 '^write:' |tail -1| awk -F' ' '{print $6}'`
         echo ${result}
         ;;
     write_exe)
-        result=`$CMD $NFS_MOUNT 1 3| grep -A 1 '^write:' |tail -1| awk -F' ' '{print $7}'`
+        result=`$CMD 1 3 $NFS_MOUNT| grep -A 1 '^write:' |tail -1| awk -F' ' '{print $7}'`
         echo ${result}
         ;;
 esac
